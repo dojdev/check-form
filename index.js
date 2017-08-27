865300e23c81a98bb6b76c72ac5cd1d82624f627
Alexey <alexey.pudnikov@yandex.ru>
2017-08-27 12:12:34 +0300

    set dummy data on DOMContentLoaded

--- index.js
+++ index.js
@@ -13,11 +13,23 @@ const MyForm = {
         }
     },
     getData: () => { },
-    setData: (Object) => { },
+    setData: (item) => {
+        fio.value = item.fio;
+        email.value = item.email;
+        phone.value = item.phone;
+    },
     submit: () => { }
 }
 
-form.addEventListener('submit', function (e) {
+document.addEventListener("DOMContentLoaded", () => {
+    MyForm.setData({
+        fio: 'dummy user data',
+        email: 'dummy@yandex.com',
+        phone: '+79111234567'
+    });
+});
+
+form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     MyForm.submit();
