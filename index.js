add5c2c1f7b5aff4509790515730d7400a435764
Alexey <alexey.pudnikov@yandex.ru>
2017-08-27 11:41:58 +0300

    add const and methods

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-var form = document.querySelector('#myForm');
+const
+    form = document.querySelector('#myForm'),
+    fio = document.querySelector('[data-field="fio"]'),
+    email = document.querySelector('[data-field="email"]'),
+    phone = document.querySelector('[data-field="phone"]'),
+    button = document.querySelector('#submitButton');
 
-form.addEventListener('submit', function () {
+const MyForm = {
+    validate: () => {
+        const status = {
+            isValid: true,
+            errorFields: []
+        }
+    },
+    getData: () => { },
+    setData: (Object) => { },
+    submit: () => { }
+}
 
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    MyForm.submit();
 });
